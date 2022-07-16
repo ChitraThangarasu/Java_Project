0fd6bec58239ef78862db0eac0c7cb09640f1ac1
chitra <chitrathangarasu@gmail.com>
2022-07-16 17:33:56 -0400

    Initial commit

---
+++ JavaProjectMavenNew/src/test/java/Samplerun.java
@@ -0,0 +1,6 @@
+public class Samplerun {
+
+    public static void main(String[] args) {
+        System.out.println("Hellow w orld");
+    }
+}
---
+++ javaprojecttest/src/Sample/Startup.java
@@ -0,0 +1,13 @@
+package Sample;
+
+public class Startup {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO world");
+
+        int a;
+        a=10; int b; b=10;
+        int result =a+b;
+        System.out.println(result);
+    }
+}
